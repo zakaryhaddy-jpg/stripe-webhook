5c76a613c3a7d41c71fd238053cc9862c814d8ff
zakaryhaddy-jpg <zakaryhaddy@gmail.com>
2026-04-29 11:20:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-app.listen(3000, () => console.log("Running"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
